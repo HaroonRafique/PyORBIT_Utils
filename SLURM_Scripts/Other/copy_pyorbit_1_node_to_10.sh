bd0068995ac7cd6cea7e2eeb93a2e9b3953ea876
Haroon Rafique <haroon.rafique@cern.ch>
2018-05-23 16:04:52 +0200

    Added SLURM Scripts

---
+++ SLURM_Scripts/Other/copy_pyorbit_1_node_to_10.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cp 1_node/pyOrbit.py 2_node
+cp 1_node/pyOrbit.py 3_node
+cp 1_node/pyOrbit.py 4_node
+cp 1_node/pyOrbit.py 5_node
+cp 1_node/pyOrbit.py 6_node
+cp 1_node/pyOrbit.py 7_node
+cp 1_node/pyOrbit.py 8_node
+cp 1_node/pyOrbit.py 9_node
+cp 1_node/pyOrbit.py 10_node
