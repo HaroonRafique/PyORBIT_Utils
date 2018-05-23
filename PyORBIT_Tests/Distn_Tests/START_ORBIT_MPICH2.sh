871e7307273218501f44b617b8185addf9f58e7b
Haroon Rafique <haroon.rafique@cern.ch>
2018-05-23 15:24:22 +0200

    Added 64x64x20 2.5D grid runs and associated plootting scripts

--- PyORBIT_Tests/Distn_Tests/START_ORBIT_MPICH2.sh
+++ PyORBIT_Tests/Distn_Tests/START_ORBIT_MPICH2.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+export PYTHONPATH=${PYTHONPATH}:${ORBIT_ROOT}/py:${ORBIT_ROOT}/lib:${ORBIT_ROOT}/virtualenvs/py2.7/bin/lib/python2.7/site-packages/numpy
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ORBIT_ROOT}/lib
+module load mpi/mpich-x86_64
+source /home/HR/Documents/PyORBIT_HB_New_Features/py-orbit/customEnvironment.sh
+source /home/HR/Documents/PyORBIT_HB_New_Features/virtualenvs/py2.7/bin/activate
+
+
 if [ ! -n "$1" ]
   then
     echo "Usage: `basename $0` <name of the SC script> <N CPUs>"
@@ -13,4 +20,4 @@ if [ ! -n "$2" ]
 fi
 
 # /afs/cern.ch/project/LIUsc/space_charge/Codes/mpich-3.0/bin/mpirun -np $2 ${ORBIT_ROOT}/bin/pyORBIT $1
-/afs/cern.ch/project/LIUsc/space_charge/Codes/mpich2-1.4.1p1/bin/mpirun -np $2 ${ORBIT_ROOT}/bin/pyORBIT $1
+mpirun -np $2 ${ORBIT_ROOT}/bin/pyORBIT $1
