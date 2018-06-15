efbe696d4c811d53b9f50fd192a986fa9b848937
Haroon Rafique <haroon.rafique@cern.ch>
2018-06-15 14:19:30 +0200

    updated clean_folder scripts in SIS18

--- SIS18/Simulation/Step2/clean_folder.sh
+++ SIS18/Simulation/Step2/clean_folder.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 rm -r input
-rm -r output
-rm Negative_node.OUT
-rm ptc_twiss
+rm output/mainbunch*
+rm -r lost
 rm simulation_parameters.pyc
 rm SIS_18_BENCHMARK.flt
+rm output.mat
+rm junk.txt
+rm Maxwellian_bend_for_ptc.txt
+rm ptc_twiss_matched.tfs
+rm twiss_sis18_matched.tfs
+rm ptc_twiss_unmatched.tfs
