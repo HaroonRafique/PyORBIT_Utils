891ed6c183fd6e6c2619f55cf78ccb128ae9a69a
Haroon Rafique <haroon.rafique@cern.ch>
2018-06-15 17:27:51 +0200

    Updated and checked SIS18 Step4

--- SIS18/Simulation/Step4/simulation_parameters.py
+++ SIS18/Simulation/Step4/simulation_parameters.py
@@ -4,7 +4,7 @@ import numpy as np
 beta = 0.15448
 gamma = 1.012149995
 m = 1.2
-intensity=3e+10            # DeltaQ = 0.1  Z=40.3m t=3472.7ns
+intensity=0.305564e11            # DeltaQ = 0.1  Z=40.3m t=3472.7ns
 # ~ intensity=2e+9             # DeltaQ = 0.1  Z=2.69m t=231.51ns
 # Assume Geometric epsn_x = (beta*gamma)*epsn_g_x
 epsn_x=(beta*gamma)*(12.57e-6)/4           # beta*gamma*e_g/4 = 4.91E-7
---
+++ SIS18/Simulation/Step5/clean_folder.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -r input
+rm output/mainbunch*
+rm -r lost
+rm simulation_parameters.pyc
+rm SIS_18_BENCHMARK.flt
+rm output.mat
+rm junk.txt
+rm Maxwellian_bend_for_ptc.txt
+rm ptc_twiss_matched.tfs
+rm twiss_sis18_matched.tfs
+rm ptc_twiss_unmatched.tfs
