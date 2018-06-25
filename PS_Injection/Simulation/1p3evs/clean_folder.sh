86b9bf2daa9fc2296e7feca3a954d4516c5ddfe9
Haroon Rafique <HR@aoru.dyndns.cern.ch>
2018-06-25 18:18:21 +0200

    PS_Injection: Updated simulations

---
+++ PS_Injection/Simulation/1p3evs/clean_folder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm flat.dat
+rm -r input
+rm -r output
+rm Negative_node.OUT
+rm ptc_twiss
+rm simulation_parameters.pyc
+rm SPACE_CHARGE_STUDIES_INJECTION.flt
+rm junk.txt
+rm Maxwellian_bend_for_ptc.txt
