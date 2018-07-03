3fe5a59ac0325f0c2fa581bd4f53b7c79ea33fea
Haroon Rafique <haroon.rafique@cern.ch>
2018-07-03 13:03:53 +0200

    PS_Injection: pickle causing output issues, try again

--- PS_Injection/Simulation/1p3evs/clean_folder.sh
+++ PS_Injection/Simulation/1p3evs/clean_folder.sh
@@ -6,8 +6,9 @@ rm SPACE_CHARGE_STUDIES_INJECTION.flt
 rm ptc_twiss
 rm Negative_node.OUT
 rm simulation_parameters.pyc
-rm -r input/
 rm -r bunch_output/mainbunch*
 mkdir Condor_Logfiles
+mv input/simulation_info_* Condor_Logfiles
+rm -r input/
 mv logfile_* Condor_Logfiles
 mv output_* Condor_Logfiles
