dff90aed594f830669dc01cb101d5e003065c902
Haroon Rafique <haroon.rafique@cern.ch>
2018-07-03 13:06:19 +0200

    PS_Injection: Added script to submit all cases to HTCondor

---
+++ PS_Injection/Simulation/submit_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd 1p3evs
+condor_submit htcond.sub &
+cd ../1p6evs
+condor_submit htcond.subb &
+cd ../1p9evs
+condor_submit htcond.subb &
+cd ../2p3evs
+condor_submit htcond.subb &
+cd ../2p6evs
+condor_submit htcond.subb &
+cd ../
