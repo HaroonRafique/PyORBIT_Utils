239c78a5caf3e65e217ac576398aef382847df09
Haroon Rafique <haroon.rafique@cern.ch>
2018-07-24 19:24:42 +0200

    PS_Injection: Added Long_Distn_Poincare for new function test

---
+++ PS_Injection/Simulation/Long_Distn_Poincare/pyOrbit_print_particle.py
@@ -0,0 +1,372 @@
+import math
+import sys
+import time
+import orbit_mpi
+import timeit
+import numpy as np
+import scipy.io as sio
+import os
+
+# Use switches in simulation_parameters.py in current folder
+#-------------------------------------------------------------
+from simulation_parameters import switches as s
+slicebyslice = s['SliceBySlice']        # 2.5D space charge
+frozen = s['Frozen']                    # Frozen space charge
+
+if frozen:
+        slicebyslice=0
+
+# utils
+from orbit.utils.orbit_mpi_utils import bunch_orbit_to_pyorbit, bunch_pyorbit_to_orbit
+from orbit.utils.consts import mass_proton, speed_of_light, pi
+
+# bunch
+from bunch import Bunch
+from bunch import BunchTwissAnalysis, BunchTuneAnalysis
+from orbit.bunch_utils import ParticleIdNumber
+
+# diagnostics
+from orbit.diagnostics import TeapotStatLatsNode, TeapotMomentsNode, TeapotTuneAnalysisNode
+from orbit.diagnostics import addTeapotDiagnosticsNodeAsChild
+from orbit.diagnostics import addTeapotMomentsNodeSet, addTeapotStatLatsNodeSet
+
+# PTC lattice
+from libptc_orbit import *
+from ext.ptc_orbit import PTC_Lattice
+from ext.ptc_orbit import PTC_Node
+from ext.ptc_orbit.ptc_orbit import setBunchParamsPTC, readAccelTablePTC, readScriptPTC
+from ext.ptc_orbit.ptc_orbit import updateParamsPTC, synchronousSetPTC, synchronousAfterPTC
+from ext.ptc_orbit.ptc_orbit import trackBunchThroughLatticePTC, trackBunchInRangePTC
+from orbit.aperture import TeapotApertureNode
+
+# transverse space charge
+# Use switches as these conflict
+#----------------------------------------------
+if frozen:
+        from orbit.space_charge.analytical import scAccNodes, scLatticeModifications
+        from spacecharge import SpaceChargeCalcAnalyticGaussian
+        from spacecharge import GaussianLineDensityProfile
+        #from spacecharge import InterpolatedLineDensityProfile
+if slicebyslice:
+        #PIC
+        from orbit.space_charge.sc2p5d import scAccNodes, scLatticeModifications
+        # ~ from spacecharge import SpaceChargeCalc2p5D, Boundary2D
+        from spacecharge import SpaceChargeCalcSliceBySlice2D
+        from spacecharge import SpaceChargeCalcAnalyticGaussian
+        from spacecharge import InterpolatedLineDensityProfile
+
+from lib.output_dictionary import *
+from lib.pyOrbit_GenerateInitialDistribution2 import *
+# ~ from lib.pyOrbit_GenerateMatchedDistribution import *
+from lib.save_bunch_as_matfile import *
+
+# MPI stuff
+#-----------------------------------------------------------------------
+comm = orbit_mpi.mpi_comm.MPI_COMM_WORLD
+rank = orbit_mpi.MPI_Comm_rank(comm)
+print 'Start on MPI process: ', rank
+
+# Create folder structure
+#-----------------------------------------------------------------------
+print '\nmkdir on MPI process: ', rank
+from lib.mpi_helpers import mpi_mkdir_p
+mpi_mkdir_p('input')
+mpi_mkdir_p('bunch_output')
+mpi_mkdir_p('output')
+mpi_mkdir_p('lost')
+
+# Dictionary for simulation status
+#-----------------------------------------------------------------------
+import pickle # HAVE TO CLEAN THIS FILE BEFORE RUNNING A NEW SIMULATION
+status_file = 'input/simulation_status.pkl'
+if not os.path.exists(status_file):
+        sts = {'turn': -1}
+else:
+        with open(status_file) as fid:
+                sts = pickle.load(fid)
+
+# Generate Lattice (MADX + PTC) - Use MPI to run on only one 'process'
+#-----------------------------------------------------------------------
+print '\nStart MADX on MPI process: ', rank
+if not rank:
+	os.system("/afs/cern.ch/eng/sl/MAD-X/pro/releases/5.02.00/madx-linux64 < Flat_file.madx")
+orbit_mpi.MPI_Barrier(comm)
+
+# Generate PTC RF table
+#-----------------------------------------------------------------------
+print '\nstart RF file on MPI process: ', rank
+from lib.write_ptc_table import write_RFtable
+from simulation_parameters import RFparameters as RF 
+write_RFtable('input/RF_table.ptc', *[RF[k] for k in ['harmonic_factors','time','Ekin_GeV','voltage_MV','phase']])
+
+
+# Initialize a Teapot-Style PTC lattice
+#-----------------------------------------------------------------------
+print '\nstart PTC Flat file on MPI process: ', rank
+PTC_File = "SPACE_CHARGE_STUDIES_INJECTION.flt"
+Lattice = PTC_Lattice("PS")
+Lattice.readPTC(PTC_File)
+
+readScriptPTC('Input/fringe.txt')
+readScriptPTC('Input/time.ptc')
+readScriptPTC('Input/ramp_cavities.ptc')
+# ~ readScriptPTC('Input/chrom.ptc')
+
+# Create a dictionary of parameters
+#-----------------------------------------------------------------------
+print '\nparamsDict on MPI process: ', rank
+paramsDict = {}
+paramsDict["length"]=Lattice.getLength()/Lattice.nHarm
+
+# Add apertures
+#-----------------------------------------------------------------------
+print '\nAdd apertures on MPI process: ', rank
+position = 0
+for node in Lattice.getNodes():
+	myaperturenode = TeapotApertureNode(1, 10, 10, position)
+	node.addChildNode(myaperturenode, node.ENTRANCE)
+	node.addChildNode(myaperturenode, node.BODY)
+	node.addChildNode(myaperturenode, node.EXIT)
+	position += node.getLength()
+
+# Make a bunch and import relevant parameters for it
+#-----------------------------------------------------------------------
+if sts['turn'] < 0:
+	print '\nBunches on MPI process: ', rank
+	bunch = Bunch()
+	setBunchParamsPTC(bunch)
+
+	from simulation_parameters import parameters as p
+	p['harmonic_number'] = Lattice.nHarm 
+	p['phi_s']           = 0
+	p['gamma']           = bunch.getSyncParticle().gamma()
+	p['beta']            = bunch.getSyncParticle().beta()
+	p['energy']          = 1e9 * bunch.mass() * bunch.getSyncParticle().gamma()
+	p['bunch_length'] = p['blength_rms']/speed_of_light/bunch.getSyncParticle().beta()*4
+	kin_Energy = bunch.getSyncParticle().kinEnergy()
+
+	print '\nbunch_orbit_to_pyorbit on MPI process: ', rank
+	for i in p:
+		print '\t', i, '\t = \t', p[i]
+
+# Create the initial distribution 
+#-----------------------------------------------------------------------
+	print '\ngenerate_initial_distribution on MPI process: ', rank
+	Particle_distribution = generate_initial_distribution(p, Lattice, output_file='input/ParticleDistribution.in', summary_file='input/ParticleDistribution_summary.txt')
+
+	# ~ orbit_mpi.MPI_Barrier(comm)
+
+	print '\bunch_orbit_to_pyorbit on MPI process: ', rank
+	bunch_orbit_to_pyorbit(paramsDict["length"], kin_Energy, Particle_distribution, bunch, p['n_macroparticles'] + 1) #read in only first N_mp particles.
+	# ~ bunch.readBunch(Particle_distribution_file)
+	
+# Add Macrosize to bunch
+#-----------------------------------------------------------------------
+	bunch.addPartAttr("macrosize")
+	map(lambda i: bunch.partAttrValue("macrosize", i, 0, p['macrosize']), range(bunch.getSize()))
+	ParticleIdNumber().addParticleIdNumbers(bunch) # Give them unique number IDs
+
+# Dump and save as Matfile
+#-----------------------------------------------------------------------
+	bunch.dumpBunch("input/mainbunch_start.dat")
+	saveBunchAsMatfile(bunch, "bunch_output/mainbunch_-000001")
+	saveBunchAsMatfile(bunch, "input/mainbunch")
+	sts['mainbunch_file'] = "input/mainbunch"
+
+# Create empty lost bunch
+#-----------------------------------------------------------------------
+	lostbunch = Bunch()
+	bunch.copyEmptyBunchTo(lostbunch)
+	lostbunch.addPartAttr('ParticlePhaseAttributes')
+	lostbunch.addPartAttr("LostParticleAttributes")	
+	saveBunchAsMatfile(lostbunch, "input/lostbunch")
+	sts['lostbunch_file'] = "input/lostbunch"
+
+# Add items to pickle parameters
+#-----------------------------------------------------------------------
+	sts['turns_max'] = p['turns_max']
+	sts['turns_update'] = p['turns_update']
+	sts['turns_print'] = p['turns_print']
+	sts['circumference'] = p['circumference']
+	if frozen:
+		sts['sc_params1'] = {'intensity': p['intensity'],
+											 'epsn_x':    p['epsn_x'],
+											 'epsn_y':    p['epsn_y'],
+											 'dpp_rms':   p['dpp_rms']}
+
+bunch = bunch_from_matfile(sts['mainbunch_file'])
+lostbunch = bunch_from_matfile(sts['lostbunch_file'])
+paramsDict["lostbunch"]=lostbunch
+paramsDict["bunch"]= bunch
+
+#############################-------------------########################
+#############################	SPACE CHARGE	########################
+#############################-------------------########################
+
+
+# Add space charge nodes - FROZEN
+#----------------------------------------------------
+if frozen:
+	# ~ ld_profile = sts['linedensity_profile']
+	# ~ LineDensity = InterpolatedLineDensityProfile(min(ld_profile[0]), max(ld_profile[0]), ld_profile[1].tolist())
+	LineDensity=GaussianLineDensityProfile(p['blength_rms'])
+	print '\nSetting up the space charge calculations ...'
+	# Make a SC solver using frozen potential
+	# ~ sc_path_length_min = 1E-8
+	sc_path_length_min = s['MinPathLength']
+	sc_params1 = {'intensity': p['intensity'], 'epsn_x': p['epsn_x'], 'epsn_y': p['epsn_y'], 'dpp_rms': p['dpp_rms'], 'LineDensity': LineDensity}
+	space_charge_solver1 = SpaceChargeCalcAnalyticGaussian(*[sc_params1[k] for k in ['intensity','epsn_x','epsn_y','dpp_rms','LineDensity']])
+	print dir(scLatticeModifications)
+	sc_nodes1 = scLatticeModifications.setSCanalyticalAccNodes(Lattice, sc_path_length_min, space_charge_solver1)
+	print 'Installed %i space charge nodes'%(len(sc_nodes1))
+
+# Add space charge nodes
+#----------------------------------------------------
+if slicebyslice:
+	print '\nAdding space charge nodes on MPI process: ', rank
+	# Make a SC solver
+	sizeX = s['SC_x_gridsize']
+	sizeY = s['SC_y_gridsize']
+	sizeZ = s['SC_z_gridsize']  # Number of longitudinal slices in the 2.5D solver
+	calcsbs = SpaceChargeCalcSliceBySlice2D(sizeX,sizeY,sizeZ)
+	sc_path_length_min = 1E-8
+	# Add the space charge solver to the lattice as child nodes
+	sc_nodes = scLatticeModifications.setSC2p5DAccNodes(Lattice, sc_path_length_min, calcsbs)
+	print '  Installed', len(sc_nodes), 'space charge nodes ...'
+
+
+# Add tune analysis child node
+#-----------------------------------------------------
+parentnode_number = 97
+parentnode = Lattice.getNodes()[parentnode_number]
+Twiss_at_parentnode_entrance = Lattice.getNodes()[parentnode_number-1].getParamsDict()
+tunes = TeapotTuneAnalysisNode("tune_analysis")
+
+tunes.assignTwiss(*[Twiss_at_parentnode_entrance[k] for k in ['betax','alphax','etax','etapx','betay','alphay','etay','etapy']])
+tunes.assignClosedOrbit(*[Twiss_at_parentnode_entrance[k] for k in ['orbitx','orbitpx','orbity','orbitpy']])
+addTeapotDiagnosticsNodeAsChild(Lattice, parentnode, tunes)
+
+if frozen:
+# Prepare a bunch object to store particle coordinates
+#-----------------------------------------------------------------------
+	print '\nBunch on MPI process: ', rank
+	bunch_tmp = Bunch()
+	bunch.copyEmptyBunchTo(bunch_tmp)
+	bunch_tmp.addPartAttr('ParticlePhaseAttributes')
+
+# Define twiss analysis and output dictionary
+#-----------------------------------------------------------------------
+print '\nTWISS on MPI process: ', rank
+bunchtwissanalysis = BunchTwissAnalysis() #Prepare the analysis class that will look at emittances, etc.
+get_dpp = lambda b, bta: np.sqrt(bta.getCorrelation(5,5)) / (b.getSyncParticle().gamma()*b.mass()*b.getSyncParticle().beta()**2)
+get_bunch_length = lambda b, bta: 4 * np.sqrt(bta.getCorrelation(4,4)) / (speed_of_light*b.getSyncParticle().beta())
+get_eps_z = lambda b, bta: 1e9 * 4 * pi * bta.getEmittance(2) / (speed_of_light*b.getSyncParticle().beta())
+
+output_file = 'output/output.mat'
+output = Output_dictionary()
+output.addParameter('turn', lambda: turn)
+output.addParameter('intensity', lambda: bunchtwissanalysis.getGlobalMacrosize())
+output.addParameter('n_mp', lambda: bunchtwissanalysis.getGlobalCount())
+output.addParameter('gamma', lambda: bunch.getSyncParticle().gamma())
+output.addParameter('mean_x', lambda: bunchtwissanalysis.getAverage(0))
+output.addParameter('mean_xp', lambda: bunchtwissanalysis.getAverage(1))
+output.addParameter('mean_y', lambda: bunchtwissanalysis.getAverage(2))
+output.addParameter('mean_yp', lambda: bunchtwissanalysis.getAverage(3))
+output.addParameter('mean_z', lambda: bunchtwissanalysis.getAverage(4))
+output.addParameter('mean_dE', lambda: bunchtwissanalysis.getAverage(5))
+output.addParameter('epsn_x', lambda: bunchtwissanalysis.getEmittanceNormalized(0))
+output.addParameter('epsn_y', lambda: bunchtwissanalysis.getEmittanceNormalized(1))
+output.addParameter('eps_z', lambda: get_eps_z(bunch, bunchtwissanalysis))
+output.addParameter('bunchlength', lambda: get_bunch_length(bunch, bunchtwissanalysis))
+output.addParameter('dpp_rms', lambda: get_dpp(bunch, bunchtwissanalysis))
+# ~ output.addParameter('turn_date', lambda: time.strftime("%Y-%m-%d"))
+# ~ output.addParameter('turn_time', lambda: time.strftime("%H:%M:%S"))
+
+if frozen:
+	output.addParameter('BE_intensity1', lambda: sc_params1['intensity'])
+	output.addParameter('BE_epsn_x1', lambda: sc_params1['epsn_x'])
+	output.addParameter('BE_epsn_y1', lambda: sc_params1['epsn_y'])
+	output.addParameter('BE_dpp_rms1', lambda: sc_params1['dpp_rms'])
+
+if os.path.exists(output_file):
+	output.import_from_matfile(output_file)
+
+# Make a dict object that looks like: particle_n {"x,y,xp,yp,z, or dE": { "turn": "value x[n]"}}
+def create_particle_dict(n):
+
+	particle_index = str(n)
+	
+	particles={}
+	
+	particle_0 = {}
+	particle_0['x'] = {}
+	# particle_n['x']['turn'] = bunch.x(n)
+	particle_0['x']['0'] = bunch.x(0)
+	particle_0['xp']['0'] = bunch.x(0)
+	particle_0['y']['0'] = bunch.x(0)
+	particle_0['yp']['0'] = bunch.x(0)
+	particle_0['z']['0'] = bunch.x(0)
+	particle_0['dE']['0'] = bunch.x(0)
+	print 'Created particle dictionary'
+	particle_0
+
+
+
+# Function takes an int n, and stores 
+# Python passes addresses so we don't need to worry about manipulating the stored data
+def store_and_print_particle_n(dictionary, n, filename, finalise):
+	
+	
+
+
+# ~ x =np.zeros((tmax ,input_params['n_macroparticles']))
+
+# ~ and then filling it in the tracking with:
+
+# ~ x[turn,i]=bunch.x(i) #(i the number of macroparticle)
+
+# ~ and after the tracking was finished I could simply save it in any format:
+
+# ~ np.savetxt('x_track.dat', x)
+
+# ~ In the bunch you can have all the (bunch.)x,xp,y,yp,z,dE
+
+
+# Track
+#-----------------------------------------------------------------------
+print '\nTracking on MPI process: ', rank
+start_time = time.time()
+last_time = time.time()
+
+for turn in range(sts['turn']+1, sts['turns_max']):
+	if not rank:
+		last_time = time.time()
+	if turn == 0:		
+		output.addParameter('turn_time', lambda: time.strftime("%H:%M:%S"))
+		output.addParameter('turn_duration', lambda: (time.time() - last_time))
+		output.addParameter('cumulative_time', lambda: (time.time() - start_time))
+		start_time = time.time()
+		print 'start time = ', start_time
+		
+	Lattice.trackBunch(bunch, paramsDict)
+	bunchtwissanalysis.analyzeBunch(bunch)  # analyze twiss and emittance	
+	
+	# subtract circumference each turn in order to reconstruct the turn number from loss position
+	if frozen:
+		map(lambda i: lostbunch.partAttrValue("LostParticleAttributes", i, 0, 
+			lostbunch.partAttrValue("LostParticleAttributes", i, 0)-p['circumference']), xrange(lostbunch.getSize()))
+		bunch.addParticlesTo(bunch_tmp)
+					
+	if turn in sts['turns_update']:
+		sts['turn'] = turn
+
+	output.update()
+	
+	if turn in sts['turns_print']:
+		saveBunchAsMatfile(bunch, "input/mainbunch")
+		saveBunchAsMatfile(bunch, "bunch_output/mainbunch_%s"%(str(turn).zfill(6)))
+		saveBunchAsMatfile(lostbunch, "lost/lostbunch_%s"%(str(turn).zfill(6)))
+		output.save_to_matfile(output_file)		        
+		if not rank:
+			with open(status_file, 'w') as fid:
+				pickle.dump(sts, fid)
