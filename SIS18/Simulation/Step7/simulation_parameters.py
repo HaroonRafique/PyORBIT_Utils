77d5c7853147644e28c721742b5304fc27c03f1e
Haroon Rafique <haroon.rafique@cern.ch>
2018-08-01 10:34:47 +0200

    SIS18: Simulations: Updated Step6 and 7

--- SIS18/Simulation/Step7/simulation_parameters.py
+++ SIS18/Simulation/Step7/simulation_parameters.py
@@ -48,11 +48,11 @@ parameters = {
 switches = {
 	'Horizontal': 1,
 	'SliceBySlice': 0,
-	'Frozen': 0,
+	'Frozen': 1,
 	'MinPathLength': 1E-8,
 	# ~ 'RestoringForce':-4.403800e-09,
 	'RestoringForce':-4.38975e-09,
-	'InitialParticleTransversePosition':0
+	'InitialParticleTransversePosition':5.1E-3
 }
 
 # these are the parameters for the PTC RF table
