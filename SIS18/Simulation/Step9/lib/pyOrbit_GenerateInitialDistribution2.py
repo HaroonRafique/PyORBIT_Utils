f041b266cc36dd1a9e69cd8efb13a97889a633ad
Haroon Rafique <haroon.rafique@cern.ch>
2018-09-05 15:02:28 +0200

    SIS18: Sims: Step9: minor mods, run for 1E6 particles

--- SIS18/Simulation/Step9/lib/pyOrbit_GenerateInitialDistribution2.py
+++ SIS18/Simulation/Step9/lib/pyOrbit_GenerateInitialDistribution2.py
@@ -313,44 +313,6 @@ def generate_initial_5mm_distribution(start, half_range, parameters, Lattice, ho
 
 	return output_file
 
-'''	if orbit_mpi.MPI_Comm_rank(orbit_mpi.mpi_comm.MPI_COMM_WORLD) == 0:
-		fid = open(output_file,"w")
-		csv_writer = csv.writer(fid, delimiter=' ')
-		for i in range(parameters['n_macroparticles']):
-			(phi[i], dE[i]) = Longitudinal_distribution.getCoordinates()
-			(x[i], xp[i], y[i], yp[i]) = Transverse_distribution.getCoordinates()
-			x[i] += closedOrbitx['x0']
-			xp[i] += closedOrbitx['xp0']
-			y[i] += closedOrbity['y0']
-			yp[i] += closedOrbity['yp0']
-			dpp = dE[i] / (parameters['energy']) / parameters['beta']**2
-			x[i] += dpp * dispersionx['etax0']
-			xp[i] += dpp * dispersionx['etapx0']	
-			y[i] += dpp * dispersiony['etay0']
-			yp[i] += dpp * dispersiony['etapy0']	
-		
-			if outputFormat == 'Orbit':
-				x[i] *= 1000.
-				xp[i] *= 1000.
-				y[i] *= 1000.
-				yp[i] *= 1000.
-				dE[i] /= 1.e9		
-				csv_writer.writerow([x[i], xp[i], y[i], yp[i], phi[i], dE[i]])
-		#	else:
-				# still need to convert from phi to z!!
-				#csv_writer.writerow([x[i], xp[i], y[i], yp[i], z[i], dE[i]])		
-		fid.close()
-
-		fid = open(summary_file, 'w')
-		parameter_list = ['circumference', 'rf_voltage', 'phi_s', 'harmonic_number', 'gamma_transition', 'n_macroparticles', 'energy', 'gamma', 'bunch_length', 'LongitudinalCut', 'LongitudinalJohoParameter', 'x0', 'xp0', 'betax0', 'alphax0', 'etax0', 'etapx0', 'y0', 'yp0', 'betay0', 'alphay0', 'etay0', 'etapy0', 'epsn_x', 'epsn_y', 'TransverseCut']
-		for key in parameter_list:
-			fid.write(key + ' = ' + str(parameters[key]) + '\n')
-		fid.close()
-
-		print '\nCreated particle distribution with ' + str(parameters['n_macroparticles']) + ' macroparticles into file: ', output_file
-	
-	return output_file'''
-
 def generate_initial_distribution_FMA(parameters, output_file = 'Input/ParticleDistribution.in', summary_file = 'Input/ParticleDistribution_summary.txt', outputFormat='Orbit', triangular_grid = True):
 
 	# twiss containers
@@ -517,19 +479,19 @@ def generate_initial_distribution_3DGaussian(parameters, Lattice, output_file = 
 		csv_writer = csv.writer(fid, delimiter=' ')
 		for i in range(parameters['n_macroparticles']):
 			
-			# Longitudinal distn - use 3 sigma as cut-off (manual)
+			# Longitudinal distn - use 5 sigma as cut-off (manual)
 			
 			outside_limits_E = True
 			while outside_limits_E:
 				dE[i] = random.gauss(0., sig_E)	# Energy in eV
-				if abs(dE[i]) < (3*sig_E):
+				if abs(dE[i]) < (5*sig_E):
 					print '\n\tdE = ', dE[i]
 					outside_limits_E = False
 			
 			outside_limits_z = True			
 			while outside_limits_z:
 				z_temp = random.gauss(0., parameters['blength_rms'])
-				if abs(z_temp) < (3*parameters['blength_rms']):			
+				if abs(z_temp) < (5*parameters['blength_rms']):			
 					print '\n\tz_temp = ', z_temp		
 					phi[i] = - z_temp * h_main / R 
 					outside_limits_z = False
