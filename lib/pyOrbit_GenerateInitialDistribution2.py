5771fabc2d15e602fae9c5b321324e594a3993b2
Haroon Rafique <haroon.rafique@protonmail.com>
2018-09-05 15:04:00 +0200

    lib: Generate initial distn 2: added 3D gaussian from SIS18 Step9

--- lib/pyOrbit_GenerateInitialDistribution2.py
+++ lib/pyOrbit_GenerateInitialDistribution2.py
@@ -646,3 +646,102 @@ def generate_initial_distribution_y0(parameters, output_file = 'Input/ParticleDi
 		print '\nCreated particle distribution with ' + str(len(x)) + ' macroparticles into file: ', output_file
 	
 	return output_file
+
+def generate_initial_distribution_3DGaussian(parameters, Lattice, output_file = 'Input/ParticleDistribution.in', summary_file = 'Input/ParticleDistribution_summary.txt', outputFormat='Orbit'):
+	
+	parameters['alphax0'] = Lattice.alphax0
+	parameters['betax0']  = Lattice.betax0
+	parameters['alphay0'] = Lattice.alphay0
+	parameters['betay0']  = Lattice.betay0
+	parameters['etax0']   = Lattice.etax0
+	parameters['etapx0']  = Lattice.etapx0
+	parameters['etay0']   = Lattice.etay0
+	parameters['etapy0']  = Lattice.etapy0
+	parameters['x0']      = Lattice.orbitx0
+	parameters['xp0']     = Lattice.orbitpx0
+	parameters['y0']      = Lattice.orbity0
+	parameters['yp0']     = Lattice.orbitpy0
+	parameters['gamma_transition'] = Lattice.gammaT
+	parameters['circumference']    = Lattice.getLength()
+	parameters['length'] = Lattice.getLength()/Lattice.nHarm
+	
+	# twiss containers
+	twissX = TwissContainer(alpha = parameters['alphax0'], beta = parameters['betax0'], emittance = parameters['epsn_x'] / parameters['gamma'] / parameters['beta'])
+	twissY = TwissContainer(alpha = parameters['alphay0'], beta = parameters['betay0'], emittance = parameters['epsn_y'] / parameters['gamma'] / parameters['beta'])
+	dispersionx = {'etax0': parameters['etax0'], 'etapx0': parameters['etapx0']}
+	dispersiony = {'etay0': parameters['etay0'], 'etapy0': parameters['etapy0']}
+	closedOrbitx = {'x0': parameters['x0'], 'xp0': parameters['xp0']} 
+	closedOrbity = {'y0': parameters['y0'], 'yp0': parameters['yp0']} 
+
+	# initialize particle arrays
+	x = np.zeros(parameters['n_macroparticles'])
+	xp = np.zeros(parameters['n_macroparticles'])
+	y = np.zeros(parameters['n_macroparticles'])
+	yp = np.zeros(parameters['n_macroparticles'])
+	phi = np.zeros(parameters['n_macroparticles'])
+	# ~ z = np.zeros(parameters['n_macroparticles'])
+	dE = np.zeros(parameters['n_macroparticles'])
+
+	# building the distributions
+	Transverse_distribution = GaussDist2D(twissX, twissY, cut_off=parameters['TransverseCut'])
+	# ~ Longitudinal_distribution = LongitudinalJohoDistributionSingleHarmonic(parameters, parameters['LongitudinalJohoParameter'])
+	
+	# We need to convert z into phi
+	h_main = np.atleast_1d(parameters['harmonic_number'])[0]
+	R = parameters['circumference'] / 2 / np.pi
+	sig_E = (parameters['dpp_rms'] * parameters['energy'] * parameters['beta']**2)
+
+	if orbit_mpi.MPI_Comm_rank(orbit_mpi.mpi_comm.MPI_COMM_WORLD) == 0:
+		fid = open(output_file,"w")
+		csv_writer = csv.writer(fid, delimiter=' ')
+		for i in range(parameters['n_macroparticles']):
+			
+			# Longitudinal distn - use 5 sigma as cut-off (manual)
+			
+			outside_limits_E = True
+			while outside_limits_E:
+				dE[i] = random.gauss(0., sig_E)	# Energy in eV
+				if abs(dE[i]) < (5*sig_E):
+					print '\n\tdE = ', dE[i]
+					outside_limits_E = False
+			
+			outside_limits_z = True			
+			while outside_limits_z:
+				z_temp = random.gauss(0., parameters['blength_rms'])
+				if abs(z_temp) < (5*parameters['blength_rms']):			
+					print '\n\tz_temp = ', z_temp		
+					phi[i] = - z_temp * h_main / R 
+					outside_limits_z = False
+							
+			(x[i], xp[i], y[i], yp[i]) = Transverse_distribution.getCoordinates()
+			x[i] += closedOrbitx['x0']
+			xp[i] += closedOrbitx['xp0']
+			y[i] += closedOrbity['y0']
+			yp[i] += closedOrbity['yp0']
+			dpp = dE[i] / (parameters['energy']) / parameters['beta']**2
+			x[i] += dpp * dispersionx['etax0']
+			xp[i] += dpp * dispersionx['etapx0']	
+			y[i] += dpp * dispersiony['etay0']
+			yp[i] += dpp * dispersiony['etapy0']	
+		
+			if outputFormat == 'Orbit':
+				x[i] *= 1000.
+				xp[i] *= 1000.
+				y[i] *= 1000.
+				yp[i] *= 1000
+				dE[i] /= 1.e9		
+				csv_writer.writerow([x[i], xp[i], y[i], yp[i], phi[i], dE[i]])
+		#	else:
+				# still need to convert from phi to z!!
+				#csv_writer.writerow([x[i], xp[i], y[i], yp[i], z[i], dE[i]])		
+		fid.close()
+
+		fid = open(summary_file, 'w')
+		parameter_list = ['circumference', 'rf_voltage', 'phi_s', 'harmonic_number', 'gamma_transition', 'n_macroparticles', 'energy', 'gamma', 'bunch_length', 'LongitudinalCut', 'LongitudinalJohoParameter', 'x0', 'xp0', 'betax0', 'alphax0', 'etax0', 'etapx0', 'y0', 'yp0', 'betay0', 'alphay0', 'etay0', 'etapy0', 'epsn_x', 'epsn_y', 'TransverseCut']
+		for key in parameter_list:
+			fid.write(key + ' = ' + str(parameters[key]) + '\n')
+		fid.close()
+
+		print '\nCreated particle distribution with ' + str(parameters['n_macroparticles']) + ' macroparticles into file: ', output_file
+	
+	return output_file
