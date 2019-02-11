5d6551f471b03b4174c111d3866048ea85a30841
Haroon Rafique <haroon.rafique@cern.ch>
2019-02-11 18:30:14 +0100

    Updated PSB Sims

--- PSB_Sims/Simulation/005_First_Attempt/Acceleration_PyORBIT/simulation_parameters.py
+++ PSB_Sims/Simulation/005_First_Attempt/Acceleration_PyORBIT/simulation_parameters.py
@@ -1,25 +1,29 @@
 import numpy as np
 
+# 160 MeV PSB
+beta_l = 0.5198122836207495
+gamma_l = 1.17057569296
+
 # ~ tomo_matfile = 'tomoscope_file/tomo_data_H_nomWorkPoint_ct_297_turns2.76.mat'
 tomo_matfile = 'tomoscope_file/PyORBIT_Tomo_file'
+
 intensity = 1.6e+13 # future ISOLDE beam
 epsn_x = 2e-6
 epsn_y = 2e-6
 TransverseCut = 5
-n_macroparticles = int(1) #50000 #500000
-macrosize = np.sum(intensity)/float(n_macroparticles)
-
-beta_l = 0.5198122836207495
-gamma_l = 1.17057569296
 
 # This is a guess - need to check
-blength_rms = (beta_l*299792458*150e-9)/4.
+#blength_rms = (beta_l*299792458*150e-9)/4.
+phase_offset = 0.0
 
-turns_max = int(10000) #10000
-turns_print = range(-1, turns_max, int(1))
+turns_max = int(1000)
+n_macroparticles = int(500)
+
+macrosize = np.sum(intensity)/float(n_macroparticles)
+turns_print = range(-1, turns_max, int(10))
 
 parameters = {
-	'blength_rms': blength_rms,
+	#'blength_rms': blength_rms,
 	'tomo_file': tomo_matfile,
 	'tomo_matfile': tomo_matfile,
 	'n_macroparticles': n_macroparticles,
@@ -29,7 +33,8 @@ parameters = {
 	'TransverseCut': TransverseCut,
 	'macrosize': macrosize,
 	'turns_max': turns_max,
-	'turns_print': turns_print
+	'turns_print': turns_print,
+	'phase_offset': phase_offset
 }
 
 # these are the paramters for the PTC RF table
@@ -37,9 +42,17 @@ harmonic_factors = [1,2] #this times the base harmonic defines the RF harmonics 
 time = np.arange(0.00000, 0.02100, 0.00100)
 ones = np.ones_like(time)
 #with acceleration
-Ekin_GeV = np.array([0.16000000, 0.16156116, 0.16312827, 0.16470131, 0.16628024, 0.16786504, 0.16945569, 0.17105215, 0.17265442, 0.17426245, 0.17587624, 0.17749574, 0.17912094, 0.18075180, 0.18238832, 0.18403046, 0.18567819, 0.18733150, 0.18899036, 0.19065474, 0.19232462])
-RF_voltage_MV = np.array([.008*ones, .006*ones]).T # in MV
-RF_phase = np.array([3.14159*ones, 0.*ones]).T
+#Ekin_GeV = np.array([0.16000000, 0.16156116, 0.16312827, 0.16470131, 0.16628024, 0.16786504, 0.16945569, 0.17105215, 0.17265442, 0.17426245, 0.17587624, 0.17749574, 0.17912094, 0.18075180, 0.18238832, 0.18403046, 0.18567819, 0.18733150, 0.18899036, 0.19065474, 0.19232462])
+
+# ~ RF_voltage_MV = np.array([.008*ones, .006*ones]).T # in MV
+# ~ RF_phase = np.array([3.14159*ones, 0.*ones]).T
+
+Ekin_GeV = np.array(np.ones_like(time)*0.05)
+
+# ~ RF_voltage_MV = np.array([.008*ones, .0*ones]).T # in MV
+RF_voltage_MV = np.array([.008*ones, .0*ones]).T # in MV
+# ~ RF_phase = np.array([np.pi*ones, 0.*ones]).T
+RF_phase = np.array([(1+np.pi)*ones, 0.*ones]).T
 
 '''
 RF_phase = np.zeros((21,2))
