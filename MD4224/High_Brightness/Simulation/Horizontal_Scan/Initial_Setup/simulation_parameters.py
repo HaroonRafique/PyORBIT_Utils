d234e949fad8b435d59f8dc165e9412f2c189dd6
Haroon Rafique <haroon.rafique@cern.ch>
2019-03-01 11:42:24 +0100

    MD4224: High Brightness initial sims updated

--- MD4224/High_Brightness/Simulation/Horizontal_Scan/Initial_Setup/simulation_parameters.py
+++ MD4224/High_Brightness/Simulation/Horizontal_Scan/Initial_Setup/simulation_parameters.py
@@ -15,7 +15,8 @@ rf_voltage = 0.0212942055190595723
 
 # Simulation Parameters
 n_macroparticles = int(5E5)
-turns_max = int(7E4)	
+# ~ turns_max = int(7E4)	
+turns_max = int(1)	
 turns_update = range(-1, turns_max, 100)
 turns_print =  range(-1, turns_max, 100)
 
