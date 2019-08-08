e3c7ab3f5b521e5fe28219763d3d958f45ff10f1
Haroon Rafique <haroon.rafique@cern.ch>
2019-08-08 12:24:28 +0200

    MD4224: HB: Sims: Inj_Bump: Tune_Test3 udpated

--- MD4224/High_Brightness/Simulation/Injection_Bump/Tune_Test3/pyOrbit.py
+++ MD4224/High_Brightness/Simulation/Injection_Bump/Tune_Test3/pyOrbit.py
@@ -392,6 +392,7 @@ orbit_mpi.MPI_Barrier(comm)
 # Plotting
 #-----------------------------------------------------------------------
 if not rank:
+	PTC_Twiss.PrintOrbitExtrema('All_Twiss')
 	PTC_Twiss.PrintAllPTCTwiss('All_Twiss')
 	TwissDict = PTC_Twiss.ReturnTwissDict()
 	TurnList = PTC_Twiss.ReturnTurnList()
