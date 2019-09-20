b8b8c76303dba056466afd37d7927642b1245a2c
Haroon Rafique <haroon.rafique@protonmail.com>
2019-09-20 14:24:59 +0200

    MD4224: HB: Sims: H_Scan_Fixed_Bump: Submit scripts fixed

--- MD4224/High_Brightness/Simulation/H_Scan_Fixed_Bump/Submit_noSC.py
+++ MD4224/High_Brightness/Simulation/H_Scan_Fixed_Bump/Submit_noSC.py
@@ -6,14 +6,14 @@ master_dir = os.getcwd()
 
 sbs_locations = []
 
-sbs_locations.append('/607_noSC')
-sbs_locations.append('/609_noSC')
-sbs_locations.append('/611_noSC')
-sbs_locations.append('/613_noSC')
-sbs_locations.append('/615_noSC')
-sbs_locations.append('/617_noSC')
-sbs_locations.append('/619_noSC')
-sbs_locations.append('/621_noSC')
+sbs_locations.append('/607_noSC/PyORBIT')
+sbs_locations.append('/609_noSC/PyORBIT')
+sbs_locations.append('/611_noSC/PyORBIT')
+sbs_locations.append('/613_noSC/PyORBIT')
+sbs_locations.append('/615_noSC/PyORBIT')
+sbs_locations.append('/617_noSC/PyORBIT')
+sbs_locations.append('/619_noSC/PyORBIT')
+sbs_locations.append('/621_noSC/PyORBIT')
 
 if sbs:
 	for loc in sbs_locations:
