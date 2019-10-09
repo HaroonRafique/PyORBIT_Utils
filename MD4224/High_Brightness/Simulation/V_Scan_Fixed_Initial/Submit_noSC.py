6a3a6157991df0aa9ebe94e6f605915e897c4005
Haroon Rafique <haroon.rafique@protonmail.com>
2019-10-09 14:32:24 +0200

    MD4224: HB: Sims: V_Scan_Fixed_Initial: submission scripts updated

--- MD4224/High_Brightness/Simulation/V_Scan_Fixed_Initial/Submit_noSC.py
+++ MD4224/High_Brightness/Simulation/V_Scan_Fixed_Initial/Submit_noSC.py
@@ -6,14 +6,14 @@ master_dir = os.getcwd()
 
 sbs_locations = []
 
-sbs_locations.append('/610_noSC/PyORBIT')
-sbs_locations.append('/612_noSC/PyORBIT')
-sbs_locations.append('/614_noSC/PyORBIT')
-sbs_locations.append('/616_noSC/PyORBIT')
-sbs_locations.append('/618_noSC/PyORBIT')
-sbs_locations.append('/620_noSC/PyORBIT')
-sbs_locations.append('/622_noSC/PyORBIT')
-sbs_locations.append('/624_noSC/PyORBIT')
+sbs_locations.append('/610_noSC')
+sbs_locations.append('/612_noSC')
+sbs_locations.append('/614_noSC')
+sbs_locations.append('/616_noSC')
+sbs_locations.append('/618_noSC')
+sbs_locations.append('/620_noSC')
+sbs_locations.append('/622_noSC')
+sbs_locations.append('/624_noSC')
 
 if sbs:
 	for loc in sbs_locations:
