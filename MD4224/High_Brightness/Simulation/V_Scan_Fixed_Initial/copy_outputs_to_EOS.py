d4d0cf144045311a29e0d65b8fcb0ea5babb887b
Haroon Rafique <haroon.rafique@protonmail.com>
2019-10-14 11:58:53 +0200

    MD4224: HB: Sims: VFI: copy_outputs_to_EOS.py updated

--- MD4224/High_Brightness/Simulation/V_Scan_Fixed_Initial/copy_outputs_to_EOS.py
+++ MD4224/High_Brightness/Simulation/V_Scan_Fixed_Initial/copy_outputs_to_EOS.py
@@ -7,36 +7,32 @@ print '\nSTARTED copy_outputs_to_EOS.py'
 case = 'V_Scan_Fixed_Initial'
 case_short = 'VF'
 
-#cp output.mat /afs/cern.ch/user/h/harafiqu/EOS/SWAN_projects/PS/From_Scratch/Simulation_Output/V_Scan_Fixed_Initial/Outputs/610_SbS_VF_output.mat
-#cp mainbunch_000874.mat /afs/cern.ch/user/h/harafiqu/EOS/SWAN_projects/PS/From_Scratch/Simulation_Output/V_Scan_Fixed_Initial/Bunch_Profiles/610_SbS_c172.mat
-#cp mainbunch_002185.mat /afs/cern.ch/user/h/harafiqu/EOS/SWAN_projects/PS/From_Scratch/Simulation_Output/V_Scan_Fixed_Initial/Bunch_Profiles/610_SbS_c175.mat
-
 def make_output_copy_command(loc, case, case_short):
-	return 'cp output.mat /afs/cern.ch/user/h/harafiqu/EOS/SWAN_projects/PS/From_Scratch/Simulation_Output/'+ str(case) + '/Outputs/' + str(loc) + '_' + str(case_short) + '_output.mat'
+	return 'cp output.mat /afs/cern.ch/user/h/harafiqu/EOS/SWAN_projects/PS/From_Scratch/Simulation_Output/'+ str(case) + '/Outputs' + str(loc) + '_' + str(case_short) + '_output.mat'
 
 def make_bunch_copy_commands(loc, case):
-	return ['cp mainbunch_000874.mat /afs/cern.ch/user/h/harafiqu/EOS/SWAN_projects/PS/From_Scratch/Simulation_Output/'+ str(case) + '/Bunch_Profiles/' + str(loc) + '_c172.mat', 'cp mainbunch_002185.mat /afs/cern.ch/user/h/harafiqu/EOS/SWAN_projects/PS/From_Scratch/Simulation_Output/'+ str(case) + '/Bunch_Profiles/' + str(loc) + '_c175.mat']
+	return ['cp mainbunch_000874.mat /afs/cern.ch/user/h/harafiqu/EOS/SWAN_projects/PS/From_Scratch/Simulation_Output/'+ str(case) + '/Bunch_Profiles' + str(loc) + '_c172.mat', 'cp mainbunch_002185.mat /afs/cern.ch/user/h/harafiqu/EOS/SWAN_projects/PS/From_Scratch/Simulation_Output/'+ str(case) + '/Bunch_Profiles' + str(loc) + '_c175.mat']
 
 # Present directory
 master_dir = os.getcwd()
 
 sbs_locations = []
 
-# ~ sbs_locations.append('/610_SbS')
+sbs_locations.append('/610_SbS')
 sbs_locations.append('/611_SbS')
-# ~ sbs_locations.append('/612_SbS')
-# ~ sbs_locations.append('/613_SbS')
-# ~ sbs_locations.append('/614_SbS')
-# ~ sbs_locations.append('/615_SbS')
-# ~ sbs_locations.append('/616_SbS')
-# ~ sbs_locations.append('/617_SbS')
-# ~ sbs_locations.append('/618_SbS')
-# ~ sbs_locations.append('/619_SbS')
-# ~ sbs_locations.append('/620_SbS')
-# ~ sbs_locations.append('/621_SbS')
-# ~ sbs_locations.append('/622_SbS')
-# ~ sbs_locations.append('/623_SbS')
-# ~ sbs_locations.append('/624_SbS')
+sbs_locations.append('/612_SbS')
+sbs_locations.append('/613_SbS')
+sbs_locations.append('/614_SbS')
+sbs_locations.append('/615_SbS')
+sbs_locations.append('/616_SbS')
+sbs_locations.append('/617_SbS')
+sbs_locations.append('/618_SbS')
+sbs_locations.append('/619_SbS')
+sbs_locations.append('/620_SbS')
+sbs_locations.append('/621_SbS')
+sbs_locations.append('/622_SbS')
+sbs_locations.append('/623_SbS')
+sbs_locations.append('/624_SbS')
 
 for loc in sbs_locations:
 	# Create a full path to output folder
