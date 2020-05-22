bd0ae15a7ee3498bcc6a67acf281cec2f6fc573d
Haroon Rafique <haroon.rafique@protonmail.com>
2020-05-22 03:18:13 +0200

    lib/pyOrbit_GenerateInitialDistribution.py added

--- lib/pyOrbit_GenerateInitialDistribution.py
+++ lib/pyOrbit_GenerateInitialDistribution.py
@@ -1082,7 +1082,14 @@ def generate_initial_distribution_from_BLonD(parameters, Lattice=None, output_fi
                         # ~ print 'ERROR: pyOrbit_GenerateInitialDistribution::generate_initial_distribution_from_BLonD'
                         # ~ print parameters['BLonD_file'], ' does not contain enough particles to fill the bunch co-ordinates'
                         # ~ exit(0)
-                        
+        
+        if len(BLonD_data['dz']) <= (parameters['n_macroparticles']-1):
+                print 'generate_initial_distribution_from_BLonD::Error: input array length', len(BLonD_data['dz']), ' does not meet number of requested particles', parameters['n_macroparticles']
+                exit(0)
+        if len(BLonD_data['dE']) <= (parameters['n_macroparticles']-1):
+                print 'generate_initial_distribution_from_BLonD::Error: input file length', len(BLonD_data['dE']), ' does not meet number of requested particles', parameters['n_macroparticles']
+                exit(0)
+                
         z = BLonD_data['dz']
         dE = (BLonD_data['dE']/ 1E9)
                         
@@ -1192,7 +1199,14 @@ def generate_initial_distribution_from_BLonD_manual_Twiss(parameters, TwissDict,
                         # ~ print 'ERROR: pyOrbit_GenerateInitialDistribution::generate_initial_distribution_from_BLonD'
                         # ~ print parameters['BLonD_file'], ' does not contain enough particles to fill the bunch co-ordinates'
                         # ~ exit(0)
-                        
+        
+        if len(BLonD_data['dz']) <= (parameters['n_macroparticles']-1):
+                print 'generate_initial_distribution_from_BLonD::Error: input array length', len(BLonD_data['dz']), ' does not meet number of requested particles', parameters['n_macroparticles']
+                exit(0)
+        if len(BLonD_data['dE']) <= (parameters['n_macroparticles']-1):
+                print 'generate_initial_distribution_from_BLonD::Error: input file length', len(BLonD_data['dE']), ' does not meet number of requested particles', parameters['n_macroparticles']
+                exit(0)
+                
         z = BLonD_data['dz']
         dE = (BLonD_data['dE']/ 1E9)
                         
